ae8266a53cfecf92b924157e17ef5673db18cf83
nazarxexe <nazaralda74@gmail.com>
2025-01-25 09:14:06 +0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "soda-material";
+import 'soda-material/dist/style.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant={"outlined"}>
+        Button
+      </Button>
+    </div>
+  );
+}
